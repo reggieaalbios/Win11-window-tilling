9874c7c47f4ff53898e5b01b08aadb66221d33f7
reggieaalbios <reggieaalbios@users.noreply.github.com>
2026-08-28 10:43:25 +0800

    Harden development bootstrap download

--- bootstrap-dev.ps1
+++ bootstrap-dev.ps1
@@ -12,7 +12,16 @@ Set-StrictMode -Version 2.0
 $ErrorActionPreference = 'Stop'
 
 $bootstrapUri = 'https://raw.githubusercontent.com/reggieaalbios/Win11-window-tilling/dev/bootstrap.ps1'
-$bootstrapSource = Invoke-RestMethod -UseBasicParsing -Uri $bootstrapUri
+
+try {
+    $bootstrapSource = Invoke-RestMethod -UseBasicParsing -Uri $bootstrapUri
+} catch {
+    throw "Could not download bootstrap script from '$bootstrapUri': $($_.Exception.Message)"
+}
+if ([string]::IsNullOrWhiteSpace([string]$bootstrapSource)) {
+    throw "Bootstrap script at '$bootstrapUri' was empty. Refusing to execute."
+}
+
 $bootstrap = [scriptblock]::Create([string]$bootstrapSource)
 $bootstrapArguments = @{
     Action = $Action
